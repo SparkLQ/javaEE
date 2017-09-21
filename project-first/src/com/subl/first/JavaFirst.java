8613b717d191b64685e46c66f82c34ba3bb7a432
LQ <liuqiang6384@163.com>
2017-09-21 17:44:16 +0800

    添加

---
+++ project-first/src/com/subl/first/JavaFirst.java
@@ -0,0 +1,15 @@
+package com.subl.first;
+
+import org.junit.Test;
+
+public class JavaFirst {
+
+	@Test
+	public void testHelloWrold() {
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+	}
+}
